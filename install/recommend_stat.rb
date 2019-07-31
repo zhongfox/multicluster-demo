455ea8b1055d27d4eb2c49919f97e4103bc0b177
foxzhong <foxzhong@tencent.com>
2019-07-31 18:52:17 +0800

    locality-load-balancing

--- install/recommend_stat.rb
+++ install/recommend_stat.rb
@@ -4,14 +4,8 @@ require "json"
 require 'net/http'
 require 'optparse'
 
+
 options = {}
-stat = {
-  v1: 0,
-  v2: 0,
-  error: 0
-}
-
-
 OptionParser.new do |opts|
   opts.banner = "Usage: recommend_stat.rb [options]"
 
@@ -36,26 +30,72 @@ if options[:count] <=  0
 end
 
 def accessMall ip
+  proxy_host = 'dev-proxy.oa.com'
+  proxy_port = 8080
   uri = URI("http://#{ip}/api/mall")
   req = Net::HTTP::Get.new(uri)
-  res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+  res = Net::HTTP::Proxy(proxy_host, proxy_port).start(uri.hostname, uri.port) {|http|
     http.request(req)
   }
+
+  # res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+  #   http.request(req)
+  # }
 
   JSON.parse(res.body)
 end
 
-options[:count].times do |i|
-  res = accessMall(options[:ip])
-  if res && res["recommend"] 
-    if res["recommend"]["banner"]
-      stat[:v2] = stat[:v2] +1
-    else
-      stat[:v1] = stat[:v1] +1
+def analyze options
+  stat = {
+    v1: 0,
+    v2: 0,
+    error: 0
+  }
+
+  options[:count].times do |i|
+    begin
+    res = accessMall(options[:ip])
+    rescue => e
+      puts e
+      stat[:error] = stat[:error] +1
+      next
     end
-  else
-    stat[:error] = stat[:error] +1
+
+    if res && res["recommend"] && res["recommend"]["products"]
+      if res["recommend"]["banner"]
+        stat[:v2] = stat[:v2] +1
+      else
+        stat[:v1] = stat[:v1] +1
+      end
+    else
+      stat[:error] = stat[:error] +1
+    end
   end
+  puts stat
+  puts
 end
-puts stat
+
+TOTAL_REPLICAS = 14
+(TOTAL_REPLICAS + 1).times do |i|
+  unhealthy = i
+  healthy = TOTAL_REPLICAS - i
+
+  scaleUnhealthy = "kubectl --context guangzhou -nbase scale deploy/recommend-unhealthy --replicas=#{unhealthy}"
+  scaleHealthy = "kubectl --context guangzhou -nbase scale deploy/recommend-v1 --replicas=#{healthy}"
+
+  puts scaleUnhealthy
+  puts scaleHealthy
+  `#{scaleUnhealthy}`
+  `#{scaleHealthy}`
+
+  # wait for  all pods ready
+  loop do
+    sleep(3)
+    wait = 'kubectl --context guangzhou -nbase get ep recommend -o jsonpath="{.subsets[0].addresses[*].ip}"'
+    ips = `#{wait}`
+    break if ips.split(' ').length == TOTAL_REPLICAS
+  end
+
+  analyze options
+end
 
